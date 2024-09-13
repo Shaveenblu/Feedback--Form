b1b07dcc59bad9b33e36a3421ff4b0e9417383cd
dhanushka <dhanushka@gmail.com>
2024-09-13 09:47:21 +0530

    color change for dropdown

--- resources/views/app/customers/form-inputs.blade.php
+++ resources/views/app/customers/form-inputs.blade.php
@@ -52,7 +52,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .select2-container--default .select2-selection--multiple .select2-selection__choice {
-            background-color: #484848;
+            background-color: #007BFF;
         }
     </style>
 @endsection
