75e6420856b8ead29f1caaa5319ea8049733cbb8
dhanushka <dhanushka@gmail.com>
2024-09-24 16:46:08 +0530

    form changes

--- resources/views/app/link_generate/form_page.blade.php
+++ resources/views/app/link_generate/form_page.blade.php
@@ -1,11 +1,104 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('content')
-    <div class="container">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>NKAR TRAVEL</title>
+</head>
+<body>
 
 
-        <h1>Customer Form</h1>
+<div class="container">
+    <div class="mt-5">
+        <div class="card p-5">
+            <p>1) Reception by the representative of nkar travels and tours</p>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="xzR5hRwvY" id="exampleRadios1" value="MCSSCK2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Excellent 😆
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="xzR5hRwvY" id="exampleRadios1" value="CCRRUT2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Good 🙂
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="xzR5hRwvY" id="exampleRadios1" value="SVHTTV2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Satisfactory 😒
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="xzR5hRwvY" id="exampleRadios1" value="TRRSC2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Unsatisfactory ☹️
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
+        <div class="card p-5">
+            <p>2) Services of NKAR Travels and Tours During Your Stay </p>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="NNJEvpDTlK" id="exampleRadios1" value="MCSSCK2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Excellent 😆
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="NNJEvpDTlK" id="exampleRadios1" value="CCRRUT2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Good 🙂
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="NNJEvpDTlK" id="exampleRadios1" value="SVHTTV2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Satisfactory 😒
+                        </label>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="NNJEvpDTlK" id="exampleRadios1" value="TRRSC2024" checked>
+                        <label class="form-check-label" for="exampleRadios1">
+                            Unsatisfactory ☹️
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-@endsection
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
