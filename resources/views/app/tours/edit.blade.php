8c250467e3702204044c2f88b411302d5e366d70
dhanushka <dhanushka@gmail.com>
2024-09-25 16:06:45 +0530

    add changes

--- resources/views/app/tours/edit.blade.php
+++ resources/views/app/tours/edit.blade.php
@@ -31,8 +31,6 @@
             </x-form>
         </div>
     </div>
-
-    @can('view-any', App\Models\guide_tour::class)
     <div class="card mt-4">
         <div class="card-body">
             <h4 class="card-title w-100 mb-2">Guides</h4>
@@ -40,6 +38,5 @@
             <livewire:tour-guides-detail :tour="$tour" />
         </div>
     </div>
-    @endcan
 </div>
 @endsection
