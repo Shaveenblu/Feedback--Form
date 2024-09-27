293c77ba6c5204f3f92eba82b4eb1acfa134713b
dhanushka <dhanushka@gmail.com>
2024-09-27 16:53:30 +0530

    javascript validation

--- public/question/js/script.js
+++ public/question/js/script.js
@@ -6,77 +6,77 @@ document.getElementById('feedbackForm').onsubmit = function(event) {
     if (hotel_0_Zv2x4x6w54.length > 0) {
         const isGroup1Checked_hotel_0_Zv2x4x6w54 = Array.from(hotel_0_Zv2x4x6w54).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_Zv2x4x6w54) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "1) Cleanliness of Rooms ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_vfZWkBvin = document.querySelectorAll('.hotel_0_vfZWkBvin');
         const isGroup1Checked_hotel_0_vfZWkBvin = Array.from(hotel_0_vfZWkBvin).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_vfZWkBvin) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "2) Cleanliness of Restaurant ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_5CT3IcShn = document.querySelectorAll('.hotel_0_5CT3IcShn');
         const isGroup1Checked_hotel_0_5CT3IcShn = Array.from(hotel_0_5CT3IcShn).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_5CT3IcShn) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "3) Cleanliness of Public Areas ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_csdufRifW = document.querySelectorAll('.hotel_0_csdufRifW');
         const isGroup1Checked_hotel_0_csdufRifW = Array.from(hotel_0_csdufRifW).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_csdufRifW) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "4) Food Presentation ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_naxn4IjaP = document.querySelectorAll('.hotel_0_naxn4IjaP');
         const isGroup1Checked_hotel_0_naxn4IjaP = Array.from(hotel_0_naxn4IjaP).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_naxn4IjaP) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "5) Food Quality ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_ddLbQ9UIF = document.querySelectorAll('.hotel_0_ddLbQ9UIF');
         const isGroup1Checked_hotel_0_ddLbQ9UIF = Array.from(hotel_0_ddLbQ9UIF).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_ddLbQ9UIF) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "6) Rooms Appearance ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_tidgy8P7u = document.querySelectorAll('.hotel_0_tidgy8P7u');
         const isGroup1Checked_hotel_0_tidgy8P7u = Array.from(hotel_0_tidgy8P7u).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_tidgy8P7u) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "7) Rooms Air-condition ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_tkHqyoi0n = document.querySelectorAll('.hotel_0_tkHqyoi0n');
         const isGroup1Checked_hotel_0_tkHqyoi0n = Array.from(hotel_0_tkHqyoi0n).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_tkHqyoi0n) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "8) Rooms Linen ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_RivzREI7t = document.querySelectorAll('.hotel_0_RivzREI7t');
         const isGroup1Checked_hotel_0_RivzREI7t = Array.from(hotel_0_RivzREI7t).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_RivzREI7t) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "9) Service Front Office ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_eu2QqeYfl = document.querySelectorAll('.hotel_0_eu2QqeYfl');
         const isGroup1Checked_hotel_0_eu2QqeYfl = Array.from(hotel_0_eu2QqeYfl).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_eu2QqeYfl) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "10) Service Restaurant ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_0_vkOIIsM3O = document.querySelectorAll('.hotel_0_vkOIIsM3O');
         const isGroup1Checked_hotel_0_vkOIIsM3O = Array.from(hotel_0_vkOIIsM3O).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_0_vkOIIsM3O) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "11) Service Housekeeping")
             event.preventDefault();  // Prevent form submission
             return false;
         }
@@ -91,77 +91,77 @@ document.getElementById('feedbackForm').onsubmit = function(event) {
     if (hotel_1_Zv2x4x6w54.length > 0) {
         const isGroup1Checked_hotel_1_Zv2x4x6w54 = Array.from(hotel_1_Zv2x4x6w54).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_Zv2x4x6w54) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "1) Cleanliness of Rooms ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_vfZWkBvin = document.querySelectorAll('.hotel_1_vfZWkBvin');
         const isGroup1Checked_hotel_1_vfZWkBvin = Array.from(hotel_1_vfZWkBvin).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_vfZWkBvin) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "2) Cleanliness of Restaurant ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_5CT3IcShn = document.querySelectorAll('.hotel_1_5CT3IcShn');
         const isGroup1Checked_hotel_1_5CT3IcShn = Array.from(hotel_1_5CT3IcShn).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_5CT3IcShn) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "3) Cleanliness of Public Areas ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_csdufRifW = document.querySelectorAll('.hotel_1_csdufRifW');
         const isGroup1Checked_hotel_1_csdufRifW = Array.from(hotel_1_csdufRifW).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_csdufRifW) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "4) Food Presentation ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_naxn4IjaP = document.querySelectorAll('.hotel_1_naxn4IjaP');
         const isGroup1Checked_hotel_1_naxn4IjaP = Array.from(hotel_1_naxn4IjaP).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_naxn4IjaP) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "5) Food Quality ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_ddLbQ9UIF = document.querySelectorAll('.hotel_1_ddLbQ9UIF');
         const isGroup1Checked_hotel_1_ddLbQ9UIF = Array.from(hotel_1_ddLbQ9UIF).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_ddLbQ9UIF) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "6) Rooms Appearance ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_tidgy8P7u = document.querySelectorAll('.hotel_1_tidgy8P7u');
         const isGroup1Checked_hotel_1_tidgy8P7u = Array.from(hotel_1_tidgy8P7u).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_tidgy8P7u) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "7) Rooms Air-condition ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_tkHqyoi0n = document.querySelectorAll('.hotel_1_tkHqyoi0n');
         const isGroup1Checked_hotel_1_tkHqyoi0n = Array.from(hotel_1_tkHqyoi0n).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_tkHqyoi0n) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "8) Rooms Linen ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_RivzREI7t = document.querySelectorAll('.hotel_1_RivzREI7t');
         const isGroup1Checked_hotel_1_RivzREI7t = Array.from(hotel_1_RivzREI7t).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_RivzREI7t) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "9) Service Front Office ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_eu2QqeYfl = document.querySelectorAll('.hotel_1_eu2QqeYfl');
         const isGroup1Checked_hotel_1_eu2QqeYfl = Array.from(hotel_1_eu2QqeYfl).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_eu2QqeYfl) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "10) Service Restaurant ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_1_vkOIIsM3O = document.querySelectorAll('.hotel_1_vkOIIsM3O');
         const isGroup1Checked_hotel_1_vkOIIsM3O = Array.from(hotel_1_vkOIIsM3O).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_1_vkOIIsM3O) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "11) Service Housekeeping")
             event.preventDefault();  // Prevent form submission
             return false;
         }
@@ -177,14 +177,14 @@ document.getElementById('feedbackForm').onsubmit = function(event) {
 
         const isGroup1Checked_hotel_2_Zv2x4x6w54 = Array.from(hotel_2_Zv2x4x6w54).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_Zv2x4x6w54) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "1) Cleanliness of Rooms ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_vfZWkBvin = document.querySelectorAll('.hotel_2_vfZWkBvin');
         const isGroup1Checked_hotel_2_vfZWkBvin = Array.from(hotel_2_vfZWkBvin).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_vfZWkBvin) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "2) Cleanliness of Restaurant ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
@@ -192,68 +192,82 @@ document.getElementById('feedbackForm').onsubmit = function(event) {
         const hotel_2_5CT3IcShn = document.querySelectorAll('.hotel_2_5CT3IcShn');
         const isGroup1Checked_hotel_2_5CT3IcShn = Array.from(hotel_2_5CT3IcShn).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_5CT3IcShn) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "3) Cleanliness of Public Areas ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_csdufRifW = document.querySelectorAll('.hotel_2_csdufRifW');
         const isGroup1Checked_hotel_2_csdufRifW = Array.from(hotel_2_csdufRifW).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_csdufRifW) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "4) Food Presentation ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_naxn4IjaP = document.querySelectorAll('.hotel_2_naxn4IjaP');
         const isGroup1Checked_hotel_2_naxn4IjaP = Array.from(hotel_2_naxn4IjaP).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_naxn4IjaP) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "5) Food Quality ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_ddLbQ9UIF = document.querySelectorAll('.hotel_2_ddLbQ9UIF');
         const isGroup1Checked_hotel_2_ddLbQ9UIF = Array.from(hotel_2_ddLbQ9UIF).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_ddLbQ9UIF) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "6) Rooms Appearance ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_tidgy8P7u = document.querySelectorAll('.hotel_2_tidgy8P7u');
         const isGroup1Checked_hotel_2_tidgy8P7u = Array.from(hotel_2_tidgy8P7u).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_tidgy8P7u) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "7) Rooms Air-condition ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_tkHqyoi0n = document.querySelectorAll('.hotel_2_tkHqyoi0n');
         const isGroup1Checked_hotel_2_tkHqyoi0n = Array.from(hotel_2_tkHqyoi0n).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_tkHqyoi0n) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "8) Rooms Linen ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_RivzREI7t = document.querySelectorAll('.hotel_2_RivzREI7t');
         const isGroup1Checked_hotel_2_RivzREI7t = Array.from(hotel_2_RivzREI7t).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_RivzREI7t) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "9) Service Front Office ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_eu2QqeYfl = document.querySelectorAll('.hotel_2_eu2QqeYfl');
         const isGroup1Checked_hotel_2_eu2QqeYfl = Array.from(hotel_2_eu2QqeYfl).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_eu2QqeYfl) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "10) Service Restaurant ?")
             event.preventDefault();  // Prevent form submission
             return false;
         }
         const hotel_2_vkOIIsM3O = document.querySelectorAll('.hotel_2_vkOIIsM3O');
         const isGroup1Checked_hotel_2_vkOIIsM3O = Array.from(hotel_2_vkOIIsM3O).some(radio => radio.checked);
         if (!isGroup1Checked_hotel_2_vkOIIsM3O) {
-            swal("Please select an option !", "1) Reception by the representative of nkar travels and tours ?")
+            swal("Please select an option !", "11) Service Housekeeping")
             event.preventDefault();  // Prevent form submission
             return false;
         }
     }else {
         return true;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return true;
 };
